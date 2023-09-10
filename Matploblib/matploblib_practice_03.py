653d72cf88df4b5a1e3e5121f80ffb73b76dffe1
gray <gray.b@kakaocorp.com>
2023-09-10 09:59:53 +0900

    matplotlob 3

--- Matploblib/matploblib_practice_03.py
+++ Matploblib/matploblib_practice_03.py
@@ -63,3 +63,23 @@ hax.plot_surface(grid_x,grid_y,z,camp='jet')
 hax.set_xlabel('y')
 hax.set_xlabel('x')
 hax.set_xlabel('z')
+
+# %% Simple Animation
+
+fig, ax = plt.subplots()
+
+x = np.array([1,2,3,4,5])
+y = np.array([1,1,1,1,1])
+
+hp, = ax.plot(x,y)
+ax.set_ybound([0,11])
+
+for i in range(0,11,1):
+    hp.set_ydata(i*y)
+    plt.pause(0.5)
+    #
+
+
+
+
+
